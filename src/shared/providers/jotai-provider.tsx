9b1783e63e265de56f69b01054eac4fce3e761b9
eder3232 <eldebar.dk@gmail.com>
2025-06-07 21:46:17 -0500

    Se actualiza el layout principal para incluir el proveedor Jotai, mejorando la gestión de estado.

---
+++ src/shared/providers/jotai-provider.tsx
@@ -0,0 +1,13 @@
+import { createStore, Provider } from 'jotai'
+
+const myStore = createStore()
+
+interface JotaiProviderProps {
+  children: React.ReactNode
+}
+
+const JotaiProvider = ({ children }: JotaiProviderProps) => (
+  <Provider store={myStore}>{children}</Provider>
+)
+
+export default JotaiProvider
